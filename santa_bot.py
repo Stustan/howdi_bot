69b8953e703a1c0278e90782ff9ff831b2cc0605
Stustan <coreyes@ya.ru>
2022-12-06 15:55:47 +0300

    рабочая версия

--- santa_bot.py
+++ santa_bot.py
@@ -8,32 +8,37 @@ logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-members_list = []
-members_id = []
+member_data = {'member_id': [['member_id'], ['member_name']]}
 pairs = {}
 
-def shuffle_members (members_list):
-    random.shuffle(members_list)
-    return members_list
+def shuffle_members (members_id):
+    random.shuffle(members_id)
+    return members_id
 
-def set_santa (members_list):
-    n = len(members_list)
-    for i in range(n):
+def set_santa (member_data):
+    members_id = dict.keys(member_data)
+    n = len(members_id)
+    print(members_id)
+    shuffle_members(members_id)
+    """for i in range(n):
         if i == n-1:
-            pairs[members_list[n-1]] = members_list[0]
+            pairs[members_id[n-1]] = members_id[0]
         else:
-            pairs[members_list[i]] = members_list[i+1]
-    return pairs
+            pairs[members_id[i]] = members_id[i+1]
+    return pairs"""
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Здравствуйте!\nЯ бот Тайного Санты!\nВы хотите участвовать в игре?")
+    await message.reply("Здравствуйте!\nЯ бот Тайного Санты!\nВы участвуете в игре!")
+    member_data['member_id'].append(message.from_user.id)
+    member_data[message.from_user.id] = [[message.from_user.id], [message.from_user.first_name]]
+    await message.answer(member_data)
 
 @dp.message_handler(commands=['shuffle'])
 async def shuffle_func(message: types.Message):
-    shuffle_members(members_list)
-    pairs = set_santa(members_list)
+    set_santa(member_data)
     await message.reply("Я замешал список участников")
+    await message.reply(pairs)
 
 """@dp.message_handler(commands=['i_am_santa_for'])
 async def dispatch_func(message: types.Message):
@@ -55,19 +60,9 @@ async def sending_func(message: types.Message):
         await bot.send_message(members_id[i], howdi_str)
 
 @dp.message_handler()
-async def add_user(message: types.Message):
+async def exception_message(message: types.Message):
     user_message = message.text
     match user_message:
-        case 'Да' | 'да':
-            await message.answer('УРА!!!\nВы теперь участвуете!')
-            members_list.append(message.from_user.first_name)
-            members_id.append(message.from_user.id)
-            await message.answer(members_list)
-            await message.answer(members_id)
-        case 'Нет' | 'нет':
-            await message.answer('Тогда нам с тобой не по пути(')
-        case 'всё' | 'Всё' | 'все' | 'Все':
-            await message.answer(members_list)
         case _:
             await message.answer('Сорян, я тебя не понял(')
 
